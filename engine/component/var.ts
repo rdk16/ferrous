cb69fbf5f50d97f8e8dff982c34759f12e2eae0b
rdk16 <lucasgdeogomes@gmail.com>
2026-09-23 17:12:40 -0400

    a

---
+++ engine/component/var.ts
@@ -0,0 +1,24 @@
+const variables = new Map<string, string | number | boolean | Date | null>();
+
+export function createVar(
+  name: string,
+  value: string | number | boolean | Date | null,
+) {
+  if (!variables.has(name)) {
+    variables.set(name, value);
+    return true;
+  }
+  return false;
+}
+
+export function updateVar(
+  name: string,
+  value: string | number | boolean | Date | null,
+) {
+  const variable = variables.get(name)
+  if (!variable) return false
+  const T = typeof variables.get(name)
+  if (typeof variable !== T) return false
+  const R = variables.set(name, value)
+  return true
+}
