9f0378eb3787e4182ed45ff2e1ffe7b3e4c61538
rdk16 <lucasgdeogomes@gmail.com>
2026-09-23 17:12:38 -0400

    a

--- engine/mod.ts
+++
@@ -1,3 +0,0 @@
-import { check } from "./component/if.ts";
-import { printf } from "./component/print.ts";
-export { check, printf };
--- readme.md
+++
@@ -1,3 +0,0 @@
-<h1>Bem vindo ao <strong>Ferrous</strong>!<h1>
-
-> Linguagem em desenvolvimento.
